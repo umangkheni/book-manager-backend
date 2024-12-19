bb892334f65de3ce2f6a99b674e8b7a922d0d4c5
CRKInfo <crkinfo01@gmail.com>
2024-12-19 23:38:03 +0530

    fix db bug

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.use(bodyParser.json());
 mongoose
     .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/bookmanager")
     .then(() => console.log("MongoDB connected")) 
-    .catch((err) => console.error(err));
+    .catch((err) => {
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1);
+    });
 
 // Routes
 app.use('/api/books', booksRouter);
